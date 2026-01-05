702e7337f84807454d5afaaa0a515d6819d7f4aa
joao-cavalcante <jxax.cavalcantx15@gmail.com>
2026-01-05 15:56:11 -0300

    Commit

--- server/config/cors.php
+++ server/config/cors.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:8000', 'http://localhost:3000', 'http://172.168.24.4', 'http://erp.optecit.com.br',],
-    'allowed_origins_patterns' => [],
+
+    'allowed_origins' => [
+        'http://127.0.0.1:8000',
+        'http://localhost:3000',
+        'http://172.168.24.4',
+        'http://erp.optecit.com.br',
+        'http://erp.optecit.com.br:3000', // ⚠️ importante se houver
+    ],
+
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, 
+    'supports_credentials' => true,
 ];
