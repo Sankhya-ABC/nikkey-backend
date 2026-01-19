a9718e3bad4d6da9ee185eceb48574dfd2640eb7
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-19 08:35:45 -0300

    feat: sorting

--- server/app/Http/Controllers/DashboardController.php
+++ server/app/Http/Controllers/DashboardController.php
@@ -9,7 +9,7 @@ use App\Services\Sankhya\SankhyaLoadRecordsService;
 use App\Services\Sankhya\SankhyaDbExplorerSPService;
 
 class DashboardController extends Controller {
-    public function getBasicData(Request $request)
+public function getBasicData(Request $request)
     {
         $dataInicio = Carbon::parse($request->query('dataInicio'))->startOfDay();
         $dataFim = Carbon::parse($request->query('dataFim'))->startOfDay();
@@ -20,7 +20,7 @@ class DashboardController extends Controller {
                 COUNT(DISTINCT(OSE.CODPARC)) AS qtdCliente,
                 COUNT(DISTINCT(OSE.CODTEC)) AS qtdTecnico   
             FROM sankhya.AD_VGFOSE OSE
-            WHERE cast(OSE.DHPREVISTA as date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
+            WHERE CAST(OSE.DHPREVISTA AS date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
         ";
 
         $service = new SankhyaDbExplorerSPService();
@@ -37,13 +37,13 @@ class DashboardController extends Controller {
 
         $sql = "
             SELECT 
-                cast(OSE.DHPREVISTA as date) as data,
+                CAST(OSE.DHPREVISTA AS date) AS data,
                 COUNT(1) AS qtdOrdemServico
             FROM sankhya.AD_VGFOSE OSE
-            WHERE cast(OSE.DHPREVISTA as date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
-            AND cast(OSE.DHPREVISTA as date)  IS NOT NULL
-            GROUP BY CAST(OSE.DHPREVISTA as date)
-            ORDER BY CAST(OSE.DHPREVISTA as date)
+            WHERE CAST(OSE.DHPREVISTA AS date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
+              AND CAST(OSE.DHPREVISTA AS date) IS NOT NULL
+            GROUP BY CAST(OSE.DHPREVISTA AS date)
+            ORDER BY CAST(OSE.DHPREVISTA AS date) ASC
         ";
 
         $service = new SankhyaDbExplorerSPService();
@@ -59,17 +59,17 @@ class DashboardController extends Controller {
         $dataFim = Carbon::parse($request->query('dataFim'))->startOfDay();
 
         $sql = "
-        SELECT 
-            OSE.CODTEC AS idTecnico,
-            OSE.NOMETEC AS nomeTecnico,
-            COUNT(1) AS qtdOrdemServico
-
-        FROM sankhya.AD_VGFOSE OSE
-        WHERE cast(OSE.DHPREVISTA as date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
-        AND cast(OSE.DHPREVISTA as date)  IS NOT NULL
-        GROUP BY
-            OSE.CODTEC,
-            OSE.NOMETEC
+            SELECT 
+                OSE.CODTEC AS idTecnico,
+                OSE.NOMETEC AS nomeTecnico,
+                COUNT(1) AS qtdOrdemServico
+            FROM sankhya.AD_VGFOSE OSE
+            WHERE CAST(OSE.DHPREVISTA AS date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
+              AND CAST(OSE.DHPREVISTA AS date) IS NOT NULL
+            GROUP BY
+                OSE.CODTEC,
+                OSE.NOMETEC
+            ORDER BY qtdOrdemServico DESC
         ";
 
         $service = new SankhyaDbExplorerSPService();
@@ -85,21 +85,23 @@ class DashboardController extends Controller {
         $dataFim = Carbon::parse($request->query('dataFim'))->startOfDay();
 
         $sql = "
-        SELECT  PRD.CODPROD AS codProduto,
+            SELECT  
+                PRD.CODPROD AS codProduto,
                 PRO.DESCRPROD AS descProduto,
                 SUM(ISNULL(PRD.QTDNEG,0)) AS qnt
-        FROM sankhya.AD_VGFOSESERPRGPRD PRD
-        INNER JOIN sankhya.TGFPRO PRO
-            ON PRO.CODPROD = PRD.CODPROD
-        WHERE PRD.NUMOS IN (
-                            SELECT OSE.NUMOS 
-                            FROM sankhya.AD_VGFOSE OSE 
-                            WHERE cast(OSE.DHPREVISTA   as date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
-                            AND cast(OSE.DHPREVISTA  as date)  IS NOT NULL
-                            )
-        GROUP BY 
-            PRD.CODPROD,
-            PRO.DESCRPROD
+            FROM sankhya.AD_VGFOSESERPRGPRD PRD
+            INNER JOIN sankhya.TGFPRO PRO
+                ON PRO.CODPROD = PRD.CODPROD
+            WHERE PRD.NUMOS IN (
+                SELECT OSE.NUMOS
+                FROM sankhya.AD_VGFOSE OSE
+                WHERE CAST(OSE.DHPREVISTA AS date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
+                  AND CAST(OSE.DHPREVISTA AS date) IS NOT NULL
+            )
+            GROUP BY 
+                PRD.CODPROD,
+                PRO.DESCRPROD
+            ORDER BY qnt DESC
         ";
 
         $service = new SankhyaDbExplorerSPService();
@@ -115,20 +117,21 @@ class DashboardController extends Controller {
         $dataFim = Carbon::parse($request->query('dataFim'))->startOfDay();
 
         $sql = "
-        SELECT 
-            OSE.NUMOS       AS numOS,
-            OSE.DHPREVISTA  AS data,
-            OSE.DHPREVISTA  AS horaInicio,
-            OSE.DHPREVISTAFIN  AS horaFim,
-            OSE.NOMEPARC    AS nomeCliente
-        FROM sankhya.AD_VGFOSE OSE
-        INNER JOIN sankhya.TGFPAR PAR
-            ON PAR.CODPARC = OSE.CODPARC
-        LEFT JOIN sankhya.TGFTPP TPP
-            ON TPP.CODTIPPARC = PAR.CODTIPPARC
-        WHERE cast(OSE.DHPREVISTA as date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
-        AND cast(OSE.DHPREVISTA as date) IS NOT NULL
-        AND cast(OSE.DHPREVISTA as date) >= CAST(GETDATE() AS DATE)
+            SELECT 
+                OSE.NUMOS AS numOS,
+                OSE.DHPREVISTA AS data,
+                OSE.DHPREVISTA AS horaInicio,
+                OSE.DHPREVISTAFIN AS horaFim,
+                OSE.NOMEPARC AS nomeCliente
+            FROM sankhya.AD_VGFOSE OSE
+            INNER JOIN sankhya.TGFPAR PAR
+                ON PAR.CODPARC = OSE.CODPARC
+            LEFT JOIN sankhya.TGFTPP TPP
+                ON TPP.CODTIPPARC = PAR.CODTIPPARC
+            WHERE CAST(OSE.DHPREVISTA AS date) BETWEEN '{$dataInicio}' AND '{$dataFim}'
+              AND CAST(OSE.DHPREVISTA AS date) IS NOT NULL
+              AND CAST(OSE.DHPREVISTA AS date) >= CAST(GETDATE() AS DATE)
+            ORDER BY OSE.DHPREVISTA ASC
         ";
 
         $service = new SankhyaDbExplorerSPService();
