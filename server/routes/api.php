174fd2de5d63e768fe34a44c3ca3d4d79feab2f1
AshileySabah <ashileysabrina2002@gmail.com>
2026-02-26 21:16:43 -0300

    feat: remover imprimir os

--- server/routes/api.php
+++ server/routes/api.php
@@ -46,7 +46,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/ordens-servico', [OrdemServicoController::class, 'index']);
     Route::get('/ordens-servico/{id}', [OrdemServicoController::class, 'getById']);
-    Route::get('/ordens-servico/imprimir/{id}', [OrdemServicoController::class, 'imprimirOS']);
 
     Route::get('/relatorios-produtividade', [RelatorioProdutividadeController::class, 'index']);
 
